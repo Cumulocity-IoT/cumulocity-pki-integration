608f9406dd8239af9f96ccbc27bb744699f67a3f
EUR\MURI <muhammadali.riaz@softwareag.com>
2022-05-27 15:18:10 +0200

    #2 Implemented a Certificate controller and service for testing registrations.

--- pkiintegration/src/main/java/com/cumulocity/pkiintegration/service/CertificateManagementService.java
+++ pkiintegration/src/main/java/com/cumulocity/pkiintegration/service/CertificateManagementService.java
@@ -2,8 +2,11 @@ package com.cumulocity.pkiintegration.service;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.cumulocity.pkiintegration.model.Registration;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,12 +16,15 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class CertificateManagementService {
 
-	private static final String BASE_URL = null;
-	private final RestTemplate resttemplate;
-	
-	public void testRequest() {
+	private final RestTemplate restTemplate;
+
+	public Registration[] getRegistrations() {
 		HttpEntity<?> httpEntity = new HttpEntity<>(null, new HttpHeaders());
-}
-	
-	
+		String url = "https://pgw.38.qa.go.nexusgroup.com/api/registrations/t4793186911343297060/est";
+		ResponseEntity<Registration[]> response = restTemplate.getForEntity(url, Registration[].class);
+		Registration[] registrations = response.getBody();
+		
+		return registrations;
+	}
+
 }
