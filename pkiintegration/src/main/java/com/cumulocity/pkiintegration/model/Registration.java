313c95a5f46fd8e027d8d7f2934f9e82fb0bd206
Ivaylo Iliev <ivaylo.iliev@softwareag.com>
2022-05-31 18:36:00 +0300

    Disable classes from Ali

--- pkiintegration/src/main/java/com/cumulocity/pkiintegration/model/Registration.java
+++ pkiintegration/src/main/java/com/cumulocity/pkiintegration/model/Registration.java
@@ -1,17 +1,17 @@
-package com.cumulocity.pkiintegration.model;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import lombok.Data;
-
-@Data
-public class Registration {
-
-	@JsonProperty("regid")
-	private String regId;
-	@JsonProperty("fqdn")
-	private String fqdn;
-	@JsonProperty("officer")
-	private String officer;
-
-}
+//package com.cumulocity.pkiintegration.model;
+//
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//
+//import lombok.Data;
+//
+//@Data
+//public class Registration {
+//
+//	@JsonProperty("regid")
+//	private String regId;
+//	@JsonProperty("fqdn")
+//	private String fqdn;
+//	@JsonProperty("officer")
+//	private String officer;
+//
+//}
--- pkiintegration/src/main/java/com/cumulocity/pkiintegration/rest/CertificateController.java
+++ pkiintegration/src/main/java/com/cumulocity/pkiintegration/rest/CertificateController.java
@@ -1,24 +1,24 @@
-package com.cumulocity.pkiintegration.rest;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.cumulocity.pkiintegration.service.CertificateManagementService;
-
-import lombok.RequiredArgsConstructor;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/certificates")
-public class CertificateController {
-	
-	private final CertificateManagementService certificateManagementService;
-	
-	@GetMapping("/registrations")
-	public ResponseEntity<?> getCertificateRegsitrations() {
-		return ResponseEntity.status(HttpStatus.OK).body(certificateManagementService.getRegistrations());
-	}
-}
+//package com.cumulocity.pkiintegration.rest;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.cumulocity.pkiintegration.service.CertificateManagementService;
+//
+//import lombok.RequiredArgsConstructor;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/certificates")
+//public class CertificateController {
+//	
+//	private final CertificateManagementService certificateManagementService;
+//	
+//	@GetMapping("/registrations")
+//	public ResponseEntity<?> getCertificateRegsitrations() {
+//		return ResponseEntity.status(HttpStatus.OK).body(certificateManagementService.getRegistrations());
+//	}
+//}
