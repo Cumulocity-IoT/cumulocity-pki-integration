b27e59de22e416784bf387a934d764185df0c0de
Ivaylo Iliev <ivaylo.iliev@softwareag.com>
2022-05-31 18:31:10 +0300

    Enable lomboc

--- pkiintegration/src/main/java/com/cumulocity/pkiintegration/rest/OperationResource.java
+++ pkiintegration/src/main/java/com/cumulocity/pkiintegration/rest/OperationResource.java
@@ -3,6 +3,9 @@ package com.cumulocity.pkiintegration.rest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/operations")
 @RequiredArgsConstructor
