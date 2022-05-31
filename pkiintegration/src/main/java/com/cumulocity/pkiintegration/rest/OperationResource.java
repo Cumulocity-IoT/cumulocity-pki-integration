e16e290214b022847b369140ece99712310bffc7
Ivaylo Iliev <ivaylo.iliev@softwareag.com>
2022-05-31 16:38:07 +0300

    #122 Initial implementation

---
+++ pkiintegration/src/main/java/com/cumulocity/pkiintegration/rest/OperationResource.java
@@ -0,0 +1,12 @@
+package com.cumulocity.pkiintegration.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/operations")
+@RequiredArgsConstructor
+@Slf4j
+public class OperationResource {
+	
+}
