3ccae4d5f91a6cd664a7fab37e17131460d731ba
Ivaylo Iliev <ivaylo.iliev@softwareag.com>
2022-06-08 18:13:17 +0300

    Extend implementation

--- pkiintegration/src/main/java/com/cumulocity/pkiintegration/service/OperationCreationService.java
+++ pkiintegration/src/main/java/com/cumulocity/pkiintegration/service/OperationCreationService.java
@@ -2,14 +2,16 @@ package com.cumulocity.pkiintegration.service;
 
 import org.springframework.stereotype.Service;
 
+import com.cumulocity.model.idtype.GId;
 import com.cumulocity.rest.representation.operation.OperationRepresentation;
 
 @Service
 public class OperationCreationService {
 	
-		public static OperationRepresentation createKeyPairCreationOpeation()	{
+		public static OperationRepresentation createKeyPairCreationOpeation(final GId deviceId)	{
 			OperationRepresentation operationRepresentation = new OperationRepresentation();
 			operationRepresentation.set("text to be added", "c8y_Command");
+			operationRepresentation.setDeviceId(deviceId);
 			return operationRepresentation;
 		}
 
